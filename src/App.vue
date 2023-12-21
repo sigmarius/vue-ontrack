a2f1bdbb75d828b7d2d9caa2fc50928be7272187
Anastasya <aamokeeva@yandex.ru>
2023-12-21 15:24:27 +0300

    Добавляет шапку

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup></script>
+<script setup>
+  import { CheckCircleIcon } from '@heroicons/vue/24/solid';
+</script>
 
 <template>
-  <h1 class="font-bold underline text-3xl">Hello world!</h1>
+  <header class="sticky top-0 z-20  p-3 flex items-center justify-between border-b bg-white">
+    <a href="#">
+      <img src="./assets/logo.png" alt="Logo" class="h-9" />
+    </a>
+
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+
 </template>
 
 <style scoped></style>
