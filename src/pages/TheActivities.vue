53d426f691dc07843f2155308decfdbdb43932b9
Anastasya <aamokeeva@yandex.ru>
2023-12-24 15:44:34 +0300

    Делает разметку страницы Активности

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,3 +1,50 @@
+<script setup>
+import { TrashIcon } from "@heroicons/vue/24/solid";
+
+import BaseButton from "@/components/BaseButton.vue";
+import BaseSelect from "@/components/BaseSelect.vue";
+
+const activities = ["Coding", "Reading", "Training"];
+
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: "0:15",
+  },
+  {
+    value: 30,
+    label: "0:30",
+  },
+  {
+    value: 45,
+    label: "0:45",
+  },
+];
+</script>
+
 <template>
-  <h1>Activities</h1>
+  <div>
+    <ul class="divide-y">
+      <li
+        v-for="activity in activities"
+        :key="activity"
+        class="p-4 flex flex-col gap-2"
+      >
+        <div class="flex items-center gap-2">
+          <BaseButton>
+            <TrashIcon class="h-8" />
+          </BaseButton>
+          <span class="text-xl truncate">{{ activity }}</span>
+        </div>
+
+        <div>
+          <BaseSelect
+            :options="periodSelectOptions"
+            placeholder="h:mm"
+            class="font-mono"
+          />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
