22ac55ce711e36766400bd17d94885210bb4cec7
Anastasya <aamokeeva@yandex.ru>
2023-12-24 15:57:05 +0300

    Декомпозиция: создает отдельный компонент для Активности

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from "vue";
+import { TrashIcon } from "@heroicons/vue/24/solid";
+
+import BaseButton from "@/components/BaseButton.vue";
+import BaseSelect from "@/components/BaseSelect.vue";
+
+defineProps({
+  activity: {
+    type: String,
+    required: true,
+  },
+});
+
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: "0:15",
+  },
+  {
+    value: 30,
+    label: "0:30",
+  },
+  {
+    value: 45,
+    label: "0:45",
+  },
+];
+
+const secondsToComplete = ref(null);
+</script>
+
+<template>
+  <li class="p-4 flex flex-col gap-2">
+    <div class="flex items-center gap-2">
+      <BaseButton>
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="text-xl truncate">{{ activity }}</span>
+    </div>
+
+    <div>
+      <BaseSelect
+        :options="periodSelectOptions"
+        placeholder="h:mm"
+        :selected="secondsToComplete"
+        @select="secondsToComplete = $event"
+        class="font-mono"
+      />
+    </div>
+  </li>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,50 +1,17 @@
 <script setup>
-import { TrashIcon } from "@heroicons/vue/24/solid";
-
-import BaseButton from "@/components/BaseButton.vue";
-import BaseSelect from "@/components/BaseSelect.vue";
+import ActivityItem from "@/components/ActivityItem.vue";
 
 const activities = ["Coding", "Reading", "Training"];
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: "0:15",
-  },
-  {
-    value: 30,
-    label: "0:30",
-  },
-  {
-    value: 45,
-    label: "0:45",
-  },
-];
 </script>
 
 <template>
   <div>
     <ul class="divide-y">
-      <li
+      <ActivityItem
         v-for="activity in activities"
         :key="activity"
-        class="p-4 flex flex-col gap-2"
-      >
-        <div class="flex items-center gap-2">
-          <BaseButton>
-            <TrashIcon class="h-8" />
-          </BaseButton>
-          <span class="text-xl truncate">{{ activity }}</span>
-        </div>
-
-        <div>
-          <BaseSelect
-            :options="periodSelectOptions"
-            placeholder="h:mm"
-            class="font-mono"
-          />
-        </div>
-      </li>
+        :activity="activity"
+      />
     </ul>
   </div>
 </template>
