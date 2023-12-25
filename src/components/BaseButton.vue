8b40933cd112584190cdcc0eee99060831deb004
Anastasya <aamokeeva@yandex.ru>
2023-12-25 07:37:50 +0300

    Добавляет скрипт инициализации модуля компонента

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,15 @@
-<script setup>
+<script>
+// выполняется только один раз при инициализации, а не для каждого созданного компонента
 import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from "@/constants";
+
+// динамические ключи заключаются в []
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
+  [BUTTON_TYPE_DANGER]: "text-white bg-red-500 enabled:hover:bg-red-600",
+};
+</script>
+
+<script setup>
 import { isButtonTypeValid } from "@/validators";
 
 defineProps({
@@ -9,12 +19,6 @@ defineProps({
     validator: isButtonTypeValid,
   },
 });
-
-// динамические ключи заключаются в []
-const typeClasses = {
-  [BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
-  [BUTTON_TYPE_DANGER]: "text-white bg-red-500 enabled:hover:bg-red-600",
-};
 </script>
 
 <template>
