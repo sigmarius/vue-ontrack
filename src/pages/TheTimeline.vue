2f6d5943d2aff106828042e83fcdb259d2297f67
Anastasya <aamokeeva@yandex.ru>
2023-12-28 09:23:05 +0300

    Реализует прокрутку к текущему часу

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import TimelineItem from "@/components/TimelineItem.vue";
 import {
   validateTimelineItems,
@@ -7,6 +8,8 @@ import {
   isTimelineItemValid,
   isActivityValid,
 } from "@/validators";
+
+import { MIDNIGHT_HOUR } from "@/constants";
 
 defineProps({
   timelineItems: {
@@ -28,17 +31,32 @@ defineProps({
 
 const emit = defineEmits({
   setTimelineItemActivity(timelineItem, activity) {
-    return [
-      isTimelineItemValid(timelineItem),
-      isActivityValid(activity),
-    ].every(Boolean);
+    return [isTimelineItemValid(timelineItem), isActivityValid(activity)].every(
+      Boolean
+    );
   },
 });
+
+const timelineItemRefs = ref([]);
+
+// хук onMounted(() => {}) позволяет выполнить код после того, как vue-компонент будет полностью готов, и все его элементы будут отрендерены в DOM-дереве
+onMounted(scrollToCurrentTimelineItem);
+
+function scrollToCurrentTimelineItem() {
+  // определяем текущий час
+  const currentHour = new Date().getHours();
+
+  // $el - получает корневой элемент vue-компонента
+  currentHour === MIDNIGHT_HOUR
+    ? document.body.scrollIntoView()
+    : timelineItemRefs.value[currentHour - 1].$el.scrollIntoView();
+}
 </script>
 
 <template>
   <div class="mt-7">
     <ul>
+      <!-- ref в html предоставляет доступ к DOM-элементу в js -->
       <TimelineItem
         v-for="timelineItem in timelineItems"
         :key="timelineItem.hour"
@@ -46,6 +64,7 @@ const emit = defineEmits({
         :activity-select-options="activitySelectOptions"
         :activities="activities"
         @select-activity="emit('setTimelineItemActivity', timelineItem, $event)"
+        ref="timelineItemRefs"
       />
     </ul>
   </div>
