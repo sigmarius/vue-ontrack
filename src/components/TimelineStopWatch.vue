8ca44b89de4687e43946a4cf996e8e7ca047e207
Anastasya <aamokeeva@yandex.ru>
2023-12-28 08:46:03 +0300

    Реализует запуск / остановку / сброс секундомера

--- src/components/TimelineStopWatch.vue
+++ src/components/TimelineStopWatch.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from "vue";
 import { ArrowPathIcon, PauseIcon, PlayIcon } from "@heroicons/vue/24/outline";
 import BaseButton from "@/components/BaseButton.vue";
 
@@ -6,23 +7,48 @@ import {
   BUTTON_TYPE_DANGER,
   BUTTON_TYPE_WARNING,
   BUTTON_TYPE_SUCCESS,
+  MILLISECONDS_IN_SECOND,
 } from "@/constants";
 
 import { isNumber } from "@/validators";
 import { formatSeconds } from "@/functions";
 
-defineProps({
+const props = defineProps({
   seconds: {
     type: Number,
     default: 0,
     validator: isNumber,
   },
 });
+
+const seconds = ref(props.seconds);
+
+// по умолчанию секундомер остановлен
+const isRunning = ref(false);
+
+function start() {
+  // isRunning хранит ссылку на таймер
+  isRunning.value = setInterval(() => {
+    seconds.value++;
+  }, MILLISECONDS_IN_SECOND);
+}
+
+function stop() {
+  clearInterval(isRunning.value);
+
+  isRunning.value = false;
+}
+
+function reset() {
+  stop();
+
+  seconds.value = 0;
+}
 </script>
 
 <template>
   <div class="w-full flex gap-2">
-    <BaseButton :type="BUTTON_TYPE_DANGER">
+    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset">
       <ArrowPathIcon class="h-8" />
     </BaseButton>
 
@@ -32,11 +58,19 @@ defineProps({
       {{ formatSeconds(seconds) }}
     </div>
 
-    <BaseButton :type="BUTTON_TYPE_WARNING">
+    <BaseButton
+      v-if="isRunning"
+      :type="BUTTON_TYPE_WARNING"
+      @click="stop"
+    >
       <PauseIcon class="h-8" />
     </BaseButton>
 
-    <BaseButton :type="BUTTON_TYPE_SUCCESS">
+    <BaseButton
+      v-else
+      :type="BUTTON_TYPE_SUCCESS"
+      @click="start"
+    >
       <PlayIcon class="h-8" />
     </BaseButton>
   </div>
