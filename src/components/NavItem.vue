3d9dec4f3e7b4ad7ed2dec17ae386f0297de7666
Anastasya <aamokeeva@yandex.ru>
2023-12-29 09:01:43 +0300

    Рефакторинг: оптимизирует компонент NavItem

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,16 +1,30 @@
-<script>
-export default {
-  // отключаем наследование атрибутов корневым элементом по умолчанию
-  // атрибуты наследуются <a> через директиву v-bind="$attrs"
-  inheritAttrs: false,
-};
+<script setup>
+import { computed } from "vue";
+
+import { currentPage, navigate } from "@/router";
+import { isPageValid } from "@/validators";
+
+// определяем константу props если ссылаемся на пропсы в теге <script>
+const props = defineProps({
+  page: {
+    type: String,
+    required: true,
+    validator: isPageValid,
+  },
+});
+
+const classes = computed(() => [
+  "p-2 flex flex-col items-center text-xs capitalize",
+  { "bg-gray-200 pointer-events-none": props.page === currentPage.value },
+]);
 </script>
 
 <template>
   <li class="flex-1">
     <a
-      v-bind="$attrs"
-      class="p-2 flex flex-col items-center text-xs capitalize"
+      :href="`#${page}`"
+      :class="classes"
+      @click="navigate(page)"
     >
       <slot></slot>
     </a>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,8 +2,6 @@
 import NavItem from "@/components/NavItem.vue";
 
 import { NAV_ITEMS } from "@/constants";
-
-import { currentPage, navigate } from "@/router";
 </script>
 
 <template>
@@ -12,11 +10,7 @@ import { currentPage, navigate } from "@/router";
       <NavItem
         v-for="(icon, page) in NAV_ITEMS"
         :key="page"
-        :href="`#${page}`"
-        :class="{
-          'bg-gray-200 pointer-events-none': page === currentPage,
-        }"
-        @click="navigate(page)"
+        :page="page"
       >
         <component :is="icon" class="h-6 w-6" />
         {{ page }}
