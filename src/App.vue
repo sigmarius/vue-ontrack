c7f5ca39fe44e9854f3e2fdc6fac720e73ba2a22
Anastasya <aamokeeva@yandex.ru>
2023-12-29 16:25:16 +0300

    Делает роутинг с помощью динамического компонента и компоненте KeepAlive

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import TheNav from "@/components/TheNav.vue";
-import TheTimeline from "@/pages/TheTimeline.vue";
-import TheActivities from "@/pages/TheActivities.vue";
-import TheProgress from "@/pages/TheProgress.vue";
 
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from "@/constants";
-
-import { currentPage } from "@/router";
+import { routes, currentPage } from "@/router";
 </script>
 
 <template>
   <TheHeader />
 
   <main class="flex flex-col flex-grow">
-    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
-
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
-
-    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+    <!-- компонент кеширования, сохраняет динамическое состояние интегрированого динамического компонента -->
+    <KeepAlive>
+      <component :is="routes[currentPage]" />
+    </KeepAlive>
   </main>
 
   <TheNav />
--- src/router.js
+++ src/router.js
@@ -1,8 +1,17 @@
 import { ref } from 'vue'
-import { PAGE_TIMELINE } from '@/constants'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants'
 import { isPageValid } from '@/validators'
+import TheTimeline from "@/pages/TheTimeline.vue";
+import TheActivities from "@/pages/TheActivities.vue";
+import TheProgress from "@/pages/TheProgress.vue";
 
 export const currentPage = ref(normalizePageHash())
+
+export const routes = {
+  [PAGE_TIMELINE]: TheTimeline,
+  [PAGE_ACTIVITIES]: TheActivities,
+  [PAGE_PROGRESS]: TheProgress
+}
 
 export function normalizePageHash() {
   const page = window.location.hash.slice(1)
