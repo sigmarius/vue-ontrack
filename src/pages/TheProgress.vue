e51d1d2ea559fa7574b31355dda7b4f9a402892d
Anastasya <aamokeeva@yandex.ru>
2023-12-31 11:18:20 +0300

    Делает разметку компонента прогресса активностей

--- src/pages/TheProgress.vue
+++ src/pages/TheProgress.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Progress</h1>
+  <ul class="divide-y">
+    <li class="p-4 flex flex-col gap-1">
+      <div class="text-xl truncate">Activity 1</div>
+      <div class="h-5 flex overflow-hidden rounded bg-neutral-200">
+        <div class="bg-red-500" style="width: 10%;"></div>
+      </div>
+
+      <div class="flex justify-between font-mono text-sm">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="p-4 flex flex-col gap-1">
+      <div class="text-xl truncate">Activity 2</div>
+      <div class="h-5 flex overflow-hidden rounded bg-neutral-200">
+        <div class="bg-yellow-500" style="width: 50%;"></div>
+      </div>
+
+      <div class="flex justify-between font-mono text-sm">
+        <span>50%</span>
+        <span>15:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="p-4 flex flex-col gap-1">
+      <div class="text-xl truncate">Activity 3</div>
+      <div class="h-5 flex overflow-hidden rounded bg-neutral-200">
+        <div class="bg-blue-500" style="width: 70%;"></div>
+      </div>
+
+      <div class="flex justify-between font-mono text-sm">
+        <span>70%</span>
+        <span>21:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="p-4 flex flex-col gap-1">
+      <div class="text-xl truncate">Activity 4</div>
+      <div class="h-5 flex overflow-hidden rounded bg-neutral-200">
+        <div class="bg-green-500" style="width: 100%;"></div>
+      </div>
+
+      <div class="flex justify-between font-mono text-sm">
+        <span>100%</span>
+        <span>30:00 / 30:00</span>
+      </div>
+    </li>
+  </ul>
 </template>
