33fb64c445c130b895616a2ec33e4128dba708fd
Anastasya <aamokeeva@yandex.ru>
2023-12-31 12:23:44 +0300

    Реализует изменение цветовой шкалы в зависимости от прогресса

--- src/components/ProgressItem.vue
+++ src/components/ProgressItem.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { computed } from "vue";
+import { getProgressColorClass } from "@/functions";
 import { getActivityProgress } from "@/activities"
 
 const props = defineProps(["index", "activity"]);
-
-const color = ["red", "yellow", "blue", "green"][props.index];
 
 const timeProgress = [
   "03:00 / 30:00",
@@ -20,7 +19,10 @@ const progress = computed(() => getActivityProgress(props.activity));
   <li class="p-4 flex flex-col gap-1">
     <div class="text-xl truncate">{{ activity.name }}</div>
     <div class="h-5 flex overflow-hidden rounded bg-neutral-200">
-      <div :class="`bg-${color}-500`" :style="`width: ${progress}%;`"></div>
+      <div
+        :class="getProgressColorClass(progress)"
+        :style="`width: ${progress}%;`"
+        ></div>
     </div>
 
     <div class="flex justify-between font-mono text-sm">
--- src/functions.js
+++ src/functions.js
@@ -1,24 +1,27 @@
 import {
   SECONDS_IN_MINUTE,
   MINUTES_IN_HOUR,
-  MILLISECONDS_IN_SECOND
-} from "@/constants";
-import { isNull } from "@/validators";
+  MILLISECONDS_IN_SECOND,
+  LOW_PERCENT,
+  MEDIUM_PERCENT,
+  HUNDRED_PERCENT
+} from '@/constants'
+import { isNull } from '@/validators'
 
 function generatePeriodSelectOptionsLabel(period) {
   // получаем количество часов в переменной
-  const hours = Math.floor(period / MINUTES_IN_HOUR).toString().padStart(2, 0);
+  const hours = Math.floor(period / MINUTES_IN_HOUR)
+    .toString()
+    .padStart(2, 0)
 
   // получаем количество минут в переменной
-  const minutes = (period % MINUTES_IN_HOUR).toString().padStart(2, 0);
+  const minutes = (period % MINUTES_IN_HOUR).toString().padStart(2, 0)
 
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes}`
 }
 
 export function normalizeSelectValue(value) {
-  return isNull(value) || isNaN(value)
-    ? value
-    : Number(value);
+  return isNull(value) || isNaN(value) ? value : Number(value)
 }
 
 export function id() {
@@ -29,26 +32,41 @@ export function id() {
 export function generatePeriodSelectOptions() {
   const periodsInMinutes = [
     15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480
-];
+  ]
 
-  return periodsInMinutes.map(period => ({
-        value: period * SECONDS_IN_MINUTE,
-        label: generatePeriodSelectOptionsLabel(period)
-      }
-    ));
-  }
+  return periodsInMinutes.map((period) => ({
+    value: period * SECONDS_IN_MINUTE,
+    label: generatePeriodSelectOptionsLabel(period)
+  }))
+}
 
 export function formatSeconds(seconds) {
-  const date = new Date();
+  const date = new Date()
 
   // количество секунд не может быть отрицательным
-  date.setTime(Math.abs(seconds) * MILLISECONDS_IN_SECOND);
+  date.setTime(Math.abs(seconds) * MILLISECONDS_IN_SECOND)
 
-  const utc = date.toUTCString();
+  const utc = date.toUTCString()
 
-  return utc.substring(utc.indexOf(":") - 2, utc.indexOf(":") + 6);
+  return utc.substring(utc.indexOf(':') - 2, utc.indexOf(':') + 6)
 }
 
 export function getCurrentHour() {
-  return new Date().getHours();
+  return new Date().getHours()
+}
+
+export function getProgressColorClass(percentage) {
+  if (percentage < LOW_PERCENT) {
+    return 'bg-red-500'
+  }
+
+  if (percentage < MEDIUM_PERCENT) {
+    return 'bg-yellow-500'
+  }
+
+  if (percentage < HUNDRED_PERCENT) {
+    return 'bg-blue-500'
+  }
+
+  return 'bg-green-500'
 }
