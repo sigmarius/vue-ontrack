aa6e579d8ccfe522fd112da4b7f0224b6eabf62b
Anastasya <aamokeeva@yandex.ru>
2024-01-04 15:11:04 +0300

    Рефакторинг: выносит функции для работы с данными из главного модуля в модуль storage

--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
 import '@/assets/main.css'
-import * as storage from "@/storage"
+import { loadState, saveState} from "@/storage"
 import { createApp } from 'vue'
 import App from './App.vue'
-import { timelineItems } from '@/timeline-items'
-import { activities } from '@/activities'
-import { isToday } from '@/time'
-
-function saveState() {
-    storage.save({
-        timelineItems: timelineItems.value,
-        activities: activities.value
-    })
-}
-
-function loadState() {
-    const state = storage.load();
-
-    // если приложение открыто в текущий день
-    timelineItems.value = isToday(new Date(state.date))
-        ? state.timelineItems
-        : timelineItems.value;
-
-    // если данных в хранилище нет, присваиваем дефолтные значения
-    activities.value = state.activities || activities.value;
-}
 
 // загружаем начальное состояние приложения
 loadState();
--- src/storage.js
+++ src/storage.js
@@ -1,15 +1,26 @@
 import { APP_NAME } from "@/constants"
-import { today } from "@/time"
+import { today, isToday } from "@/time"
+import { timelineItems } from '@/timeline-items'
+import { activities } from '@/activities'
 
-export function load() {
-    const state = localStorage.getItem(APP_NAME)
-
-    return state ? JSON.parse(state) : {}
+export function saveState() {
+    localStorage.setItem(APP_NAME, JSON.stringify({
+        timelineItems: timelineItems.value,
+        activities: activities.value,
+        date: today()
+    }))
 }
 
-export function save(data) {
-    localStorage.setItem(APP_NAME, JSON.stringify({
-        ...data,
-        date: today().toDateString()
-    }))
+export function loadState() {
+    const serializedState = localStorage.getItem(APP_NAME)
+
+    const state = serializedState ? JSON.parse(serializedState) : {}
+
+    // если приложение открыто в текущий день
+    timelineItems.value = isToday(new Date(state.date))
+        ? state.timelineItems
+        : timelineItems.value;
+
+    // если данных в хранилище нет, присваиваем дефолтные значения
+    activities.value = state.activities || activities.value;
 }
